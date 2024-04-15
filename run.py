bf98fbd41daf1601d8255e797418b6087c2ca461
RimPeter <hull@localappliancerentals.co.uk>
2024-04-15 14:05:09 +0100

    Ass index and about html

--- run.py
+++ run.py
@@ -1,11 +1,15 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(
